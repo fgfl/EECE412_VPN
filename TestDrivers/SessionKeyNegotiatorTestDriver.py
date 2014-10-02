ff96f569e576a442600c3250a47c310534da4996
MacKenzie <mackenzieajudd@gmail.com>
2014-10-02 11:29:53 -0700

    Added some key negotiation stuff using Diffie-Hellman.

--- TestDrivers/SessionKeyNegotiatorTestDriver.py
+++ TestDrivers/SessionKeyNegotiatorTestDriver.py
@@ -4,17 +4,18 @@ testNeg1 = SessionKeyNegotiator()
 
 testNeg2 = SessionKeyNegotiator()
 
-test1 = testNeg1.get_negotiation_message()
-print "Negotiation 1"
-print test1
+while 1:
+    test1 = testNeg1.get_negotiation_message()
+    print "Negotiation 1"
+    print test1
 
-test2 = testNeg2.get_negotiation_message()
-print "Negotiation 2"
-print test2
+    test2 = testNeg2.get_negotiation_message()
+    print "Negotiation 2"
+    print test2
 
-key1 = testNeg1.get_session_key(test2)
-key2 = testNeg2.get_session_key(test1)
+    key1 = testNeg1.get_session_key(test2)
+    key2 = testNeg2.get_session_key(test1)
 
-print "Keys (Should be same)"
-print key1
-print key2
+    print "Keys (Should be same)"
+    print key1
+    print key2
